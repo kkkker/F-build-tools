d475e71df146022c65b244c825e85130b24934f2
kker <907765631@qq.com>
2020-11-26 20:43:31 +0800

    [Mamingjun] feat: implement project

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+var mountNode = document.getElementById("root");
+ReactDOM.render(<p className="hello">Hello World!</p>, mountNode);
